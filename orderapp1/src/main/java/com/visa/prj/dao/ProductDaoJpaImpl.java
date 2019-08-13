9c9e14bb1413a56ebe83c93ce523e4e9469fdfde
Hrishikesh-Thakkar <kesh5210@gmail.com>
2019-08-13 11:18:02 +0000

    Day 8 of Training

---
+++ orderapp1/src/main/java/com/visa/prj/dao/ProductDaoJpaImpl.java
@@ -0,0 +1,36 @@
+package com.visa.prj.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import com.visa.prj.entity.Product;
+
+@Repository
+public class ProductDaoJpaImpl implements ProductDao {
+
+	@PersistenceContext
+	private EntityManager em;
+	@Override
+	public List<Product> getProducts() {
+		
+		return null;
+	}
+
+	@Override
+	public Product getProduct(int id) {
+		return em.find(Product.class, id);
+	}
+
+	@Override
+	public int addProduct(Product p) {
+		System.out.println("Product Saved");
+		em.persist(p);
+		return p.getId();
+		//return 0;
+	}
+
+}
